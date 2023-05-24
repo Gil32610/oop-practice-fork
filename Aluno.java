d920fca2dee23765d17e2a4e55d0599ce0658540
Guilherme Melo <112731659+GuiMelo012x@users.noreply.github.com>
2023-05-24 19:55:09 -0300

    Add files via upload

--- Aluno.java
+++ Aluno.java
@@ -1,17 +1,3 @@
 public class Aluno {
-    private String nome;
-    private int idade;
-    public void setNome(String nome){
-        this.nome = nome;
-    }
-    public String getNome(){
-        return nome;
-    }
-    public void setIdade(int idade){
-        this.idade = idade;
-    }
-    public int getIdade(){
-        return idade;
-    }
-}
     
+}
