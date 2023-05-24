ddf533a56780c548bc2cf299b4e14cf73cbf3025
Guilherme Melo <112731659+GuiMelo012x@users.noreply.github.com>
2023-05-24 19:54:43 -0300

    Add files via upload

---
+++ AlunoTestar.java
@@ -0,0 +1,8 @@
+public class AlunoTestar {
+    public static void main(String[] args) {
+        Aluno aluno1 = new Aluno();
+            aluno1.setNome("Gui");
+            aluno1.setIdade(21);
+            System.out.println(aluno1.getNome() + " " + aluno1.getIdade());
+        }
+}
