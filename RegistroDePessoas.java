7ed336dc2115902fe5cecb97251702e719ad74f9
Guilherme Melo <112731659+GuiMelo012x@users.noreply.github.com>
2023-05-27 20:04:31 -0300

    Add files via upload

---
+++ RegistroDePessoas.java
@@ -0,0 +1,13 @@
+public class RegistroDePessoas {
+    public static void main(String[] args) {
+        Pessoa p1 = new Pessoa();
+        Pessoa p2 = new Pessoa();
+        //Precisa chamar o metodo SET no main para poder imprimir.
+            p1.setNome();
+            //p2.setNome();
+            p1.setSexo();
+            p1.setIdade();
+        //System.out.println("TESTE DE PRINT: " + p1.getNome() + " e " + p2.getNome()); //Apenas p1 vai ter o nome, pois p2 nao está puxando o set.Nome.
+        System.out.println(p1.getNome()+ ", do sexo " + p1.getSexo() + ", possui " + p1.getIdade() + " anos.");
+    }
+}
